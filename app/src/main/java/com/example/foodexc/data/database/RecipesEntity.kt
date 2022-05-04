1c020bfd9e66651f3e6cb991ff7b0d119c0cd19f
Nikolas Stavrou <nstavr04@ucy.ac.cy>
2022-05-04 21:16:40 +0200

    Started implementing the ROOM database

---
+++ app/src/main/java/com/example/foodexc/data/database/RecipesEntity.kt
@@ -0,0 +1,14 @@
+package com.example.foodexc.data.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.foodexc.models.FoodRecipe
+import com.example.foodexc.util.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity (
+    var foodRecipe: FoodRecipe
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+}
