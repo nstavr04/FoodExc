36fcbb6e491ae120799913e5914a52486ae8c804
apatti01 <apatti01@ucy.ac.cy>
2022-05-05 19:38:58 +0200

    Updated RecipesBottomSheet.kt

---
+++ app/src/main/java/com/example/foodexc/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
@@ -0,0 +1,19 @@
+package com.example.foodexc.ui.fragments.recipes.bottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.foodexc.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class RecipesBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+    }
+}
