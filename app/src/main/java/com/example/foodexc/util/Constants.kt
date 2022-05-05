f048dfecb8c752a1893f1e357709083ec364f5b4
Nikolas Stavrou <nstavr04@ucy.ac.cy>
2022-05-05 20:42:53 +0200

    Started implementing the bottom sheet. Pills can be checked and the checked ones change color as well

--- app/src/main/java/com/example/foodexc/util/Constants.kt
+++ app/src/main/java/com/example/foodexc/util/Constants.kt
@@ -12,7 +12,7 @@ class Constants {
         const val QUERY_NUMBER = "number"
         const val QUERY_API_KEY = "apiKey"
         const val QUERY_TYPE = "type"
-        const val QUERY_CHEAP = "diet"
+        const val QUERY_CHEAP = "cuisine"
         const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
         const val QUERY_FILL_INGREDIENTS = "fillIngredients"
 
