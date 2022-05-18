dbafc36dcc1e3072c953240322f614ee2fd7613e
apatti01 <apatti01@ucy.ac.cy>
2022-05-18 20:16:00 +0200

    Removed function to convert Int to String and replaced it with @String.valueOf(..)

--- app/src/main/java/com/example/foodexc/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/example/foodexc/bindingadapters/RecipesRowBinding.kt
@@ -47,20 +47,6 @@ class RecipesRowBinding {
             }
         }
 
-        // Used so we can call setNumberOfLikes on the xml layout so we can convert the likes result
-        // from an INT that the API sends us to a String
-        // Make it static so we can access it elsewhere
-        @BindingAdapter("setNumberOfLikes")
-        @JvmStatic
-        fun setNumberOfLikes(textView: TextView, likes: Int){
-            textView.text = likes.toString()
-        }
-
-        @BindingAdapter("setNumberOfMinutes")
-        @JvmStatic
-        fun setNumberOfMinutes(textView: TextView, minutes: Int){
-            textView.text = minutes.toString()
-        }
 
         // API sends us if a recipe is cheap.
         // We use the function to trigger the color on the costly image and text view of the
