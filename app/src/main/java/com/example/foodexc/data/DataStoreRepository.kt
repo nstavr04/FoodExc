38424378e845c4c9f4addd1d60f8d6b80167b0d8
apatti01 <apatti01@ucy.ac.cy>
2022-05-18 20:59:20 +0200

    Updated dataStore to alpha06

--- app/src/main/java/com/example/foodexc/data/DataStoreRepository.kt
+++ app/src/main/java/com/example/foodexc/data/DataStoreRepository.kt
@@ -3,10 +3,7 @@ package com.example.foodexc.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.*
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.emptyPreferences
-import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.core.*
 import com.example.foodexc.util.Constants.Companion.DEFAULT_CUISINE_TYPE
 import com.example.foodexc.util.Constants.Companion.DEFAULT_MEAL_TYPE
 import com.example.foodexc.util.Constants.Companion.PREFERENCES_CUISINE_TYPE
@@ -22,14 +19,18 @@ import kotlinx.coroutines.flow.map
 import java.io.IOException
 import javax.inject.Inject
 
+
+
 @ActivityRetainedScoped
 class DataStoreRepository @Inject constructor(@ApplicationContext private val context: Context) {
 
+
+
     private object PreferenceKeys {
-        val selectedMealType = preferencesKey<String>(PREFERENCES_MEAL_TYPE)
-        val selectedMealTypeId = preferencesKey<Int>(PREFERENCES_MEAL_TYPE_ID)
-        val selectedCuisineType = preferencesKey<String>(PREFERENCES_CUISINE_TYPE)
-        val selectedCuisineTypeId = preferencesKey<Int>(PREFERENCES_CUISINE_TYPE_ID)
+        val selectedMealType = stringPreferencesKey(PREFERENCES_MEAL_TYPE)
+        val selectedMealTypeId = intPreferencesKey(PREFERENCES_MEAL_TYPE_ID)
+        val selectedCuisineType = stringPreferencesKey(PREFERENCES_CUISINE_TYPE)
+        val selectedCuisineTypeId = intPreferencesKey(PREFERENCES_CUISINE_TYPE_ID)
     }
 
     private val dataStore: DataStore<Preferences> = context.createDataStore(name = PREFERENCES_NAME)
